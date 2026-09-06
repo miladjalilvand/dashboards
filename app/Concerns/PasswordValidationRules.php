e8618f624c97c1c4d2181e4e63f9b43c3a4b1cfb
miladjalilvand <miladjalilvand3666@gmail.com>
2026-09-06 21:16:19 +0330

    pass issuse

--- app/Concerns/PasswordValidationRules.php
+++ app/Concerns/PasswordValidationRules.php
@@ -11,9 +11,18 @@ trait PasswordValidationRules
      *
      * @return array<int, \Illuminate\Contracts\Validation\Rule|array<mixed>|string>
      */
+//    protected function passwordRules(): array
+//    {
+//        return ['required', 'string', Password::default(), 'confirmed'];
+//    }
     protected function passwordRules(): array
     {
-        return ['required', 'string'];
+        return [
+            'required',
+            'string',
+            'min:8',
+            'confirmed',
+        ];
     }
 
     /**
